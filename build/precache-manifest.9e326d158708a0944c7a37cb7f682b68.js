44965a4a961f835e6649c6f484cb531b8b8364e1
Aleks700 <55395706+Aleks700@users.noreply.github.com>
2019-12-02 00:02:59 +0300

    Add files via upload

---
+++ build/precache-manifest.9e326d158708a0944c7a37cb7f682b68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adf8ab6b6e375a2ce34c8c9accf1220",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02a06471e6b36151f066",
+    "url": "/static/css/main.b169f47f.chunk.css"
+  },
+  {
+    "revision": "58a0c830a1df3bb93cd9",
+    "url": "/static/js/2.ab29a70b.chunk.js"
+  },
+  {
+    "revision": "02a06471e6b36151f066",
+    "url": "/static/js/main.84d270be.chunk.js"
+  },
+  {
+    "revision": "1f3c89d7d8e9d2ccca39",
+    "url": "/static/js/runtime-main.e89362ac.js"
+  }
+]);
